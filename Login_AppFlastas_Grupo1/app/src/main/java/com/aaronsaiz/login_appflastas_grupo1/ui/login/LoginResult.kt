e05c2c649b9e9c6328806a680482d4112c6440bc
AaronDAM2 <aaronsg2001@gmail.com>
2023-02-08 17:27:48 +0100

    Crear pantalla de login

---
+++ Login_AppFlastas_Grupo1/app/src/main/java/com/aaronsaiz/login_appflastas_grupo1/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.aaronsaiz.login_appflastas_grupo1.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
