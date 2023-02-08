ccea21a6dfdd1a077fee0056c192dba3defc8bc5
AaronDAM2 <aaronsg2001@gmail.com>
2023-02-08 16:00:46 +0100

    Interfaz Grafica Movil

---
+++ AppProyectoFaltas_Grupo1/app/src/main/java/com/aaronsaiz/appproyectofaltas_grupo1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aaronsaiz.appproyectofaltas_grupo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
